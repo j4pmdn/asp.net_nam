e447f2f4e80bbae06aa77873ce34f129e4ce6dd6
Huy Doan <doanvanhuydhdtu@gmail.com>
2025-01-06 16:07:18 +0700

    updated

---
+++ RoutingCSharp/RoutingCSharp/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoutingCSharp.Models;
+
+namespace RoutingCSharp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string id)
+        {
+            return View(new Product()
+            {
+                Id = id,
+                Name = $"Product Name: {id}"
+            });
+        }
+    }
+}
---
+++ RoutingCSharp/RoutingCSharp/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace RoutingCSharp.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
